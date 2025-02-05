a54e7f7fdac400a1fa501445aef789a6cbff3e57
WaLLikWaLi <122389210+WaLLikWaLi@users.noreply.github.com>
2025-02-05 15:39:00 +0200

    fex

--- firebase.js
+++ firebase.js
@@ -4,15 +4,7 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9rVbpvS6Mv2grr__4AiXPkMKvduPRCW4",
-  authDomain: "myprojectreact-432ae.firebaseapp.com",
-  databaseURL:
-    "https://myprojectreact-432ae-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "myprojectreact-432ae",
-  storageBucket: "myprojectreact-432ae.appspot.com",
-  messagingSenderId: "971856546009",
-  appId: "1:971856546009:web:e12547da63e5c630d3d6c8",
-  measurementId: "G-0Y02WKDHJT",
+
 };
 
 export const app = initializeApp(firebaseConfig);
